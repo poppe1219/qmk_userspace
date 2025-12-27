4b70dcaabcdbfb3629a376fe360bdce9505be9b5
poppe1219 <robin.asen@gmail.com>
2025-12-27 23:34:30 +0100

    Test reducing build size. Add unicode support and right alt button to layer 0.

--- keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
@@ -47,7 +47,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_LGUI,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RGUI,
+       KC_LGUI,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RALT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RCTL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
--- keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/rules.mk
+++ keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/rules.mk
@@ -1,1 +1,18 @@
 VIA_ENABLE = yes
+UNICODE_ENABLE = yes
+# Enable Link Time Optimization for smaller firmware
+LTO_ENABLE = yes
+# Disable unused stuff and shrink the build.
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+AUDIO_ENABLE = no
+MIDI_ENABLE = no
+EXTRAKEY_ENABLE = no # Audio and system control keys.
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+EXTRAKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+AVR_USE_MINIMAL_PRINTF = yes
