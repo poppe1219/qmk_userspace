3fce0e968c0c8d1012c3aab1d528a59c283a6ec9
poppe1219 <robin.asen@gmail.com>
2025-12-30 20:23:57 +0100

    Remove extra key from chordal map.

--- keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
@@ -48,7 +48,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                       '*', '*', '*',  '*', '*', '*'
+                       '*', '*', '*',  '*', '*'
     );
 
 // clang-format off
