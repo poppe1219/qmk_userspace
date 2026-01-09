6e88c0444c863ef822c4d706d68ef038d7a97a62
poppe1219 <robin.asen@gmail.com>
2026-01-09 20:24:35 +0100

    Added missing definitions

--- keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
@@ -52,26 +52,23 @@ enum charybdis_keymap_layers {
 #define HR_O LGUI_T(KC_O)
 #define HR_H ALGR_T(KC_H)
 
-// Home Row Mods Num Layer Left
-#define HR_PAST LGUI_T(KC_PAST)
-#define HR_QUOT LALT_T(KC_QUOT)
-#define HR_SCLN LCTL_T(KC_SCLN)
-#define HR_LBRC LSFT_T(KC_LBRC)
+// Home Row Mods Sym Layer Right
+#define HR_QUOT LSFT_T(KC_QUOT)
+#define HR_LBRC LCTL_T(KC_LBRC)
+#define HR_RBRC LALT_T(KC_RBRC)
+#define HR_EQL LGUI_T(KC_EQL)
+#define HR_MINS ALGR_T(KC_MINS)
 
-// Home Row Mods Num Layer Right
-#define HR_RBRC LSFT_T(KC_RBRC)
-#define HR_BSLS LCTL_T(KC_BSLS)
-#define HR_EQL LALT_T(KC_EQL)
-#define HR_PSLS LGUI_T(KC_PSLS)
-
-// Home Row Mods Base Layer Left
-#define HR_4 LALT_T(KC_4)
-#define HR_5 LCTL_T(KC_5)
-#define HR_6 LSFT_T(KC_6)
+// Home Row Mods Sym Layer Left
+#define HR_0 LGUI_T(KC_0)
+#define HR_3 LALT_T(KC_3)
+#define HR_2 LCTL_T(KC_2)
+#define HR_1 LSFT_T(KC_1)
+#define HR_4 ALGR_T(KC_4)
 
 // Thumb Row Mods
-#define SPC_NUM LT(L_SYM, KC_SPC)
-#define ENT_NUM LT(L_SYM, KC_ENT)
+#define SPC_SYM LT(L_SYM, KC_SPC)
+#define ENT_SYM LT(L_SYM, KC_ENT)
 #define ESC_NV2 LT(L_NV, KC_ESC)
 #define TAB_NV2 LT(L_NV, KC_TAB)
 
@@ -108,23 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_QUOT,    HR_A,    HR_R,    HR_S,    HR_T,    KC_G,       KC_M,    HR_N,    HR_E,    HR_I,    HR_O, KC_MINS,
      MO(L_PTR),    KC_Z,    KC_X,    KC_C,    HR_D,    KC_V,       KC_K,    HR_H, KC_COMM,  KC_DOT, KC_SLSH,  QK_REP,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_BSPC, SPC_NUM, ESC_NV2,    TAB_NV2, ENT_NUM
+                                  KC_BSPC, SPC_SYM, ESC_NV2,    TAB_NV2, ENT_SYM
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
-  //[L_NUM] = LAYOUT(
-  //// ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-  //     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_NUM,
-  //     QK_LLCK, HR_PAST, HR_QUOT, HR_SCLN, HR_LBRC, KC_LPRN,    KC_RPRN, HR_RBRC, HR_BSLS,  HR_EQL, HR_PSLS, _______,
-  //     XXXXXXX, XXXXXXX, KC_PMNS, KC_PPLS, KC_ALGR, XXXXXXX,    KC_HASH, KC_ALGR, _______, _______, _______, _______,
-  //// ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-  //                                _______, _______,  KC_ESC,     KC_TAB, _______
-  ////                            ╰───────────────────────────╯ ╰──────────────────╯
-  //),
   [L_SYM] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_GRV,    KC_0,    KC_8,    KC_8,    KC_7, XXXXXXX,    XXXXXXX, KC_PAST, KC_LPRN, KC_RPRN, _______,  _______,
        QK_LLCK,    HR_0,    HR_3,    HR_2,    HR_1, XXXXXXX,    XXXXXXX, HR_QUOT, HR_LBRC, HR_RBRC,  HR_EQL, XXXXXXX,
-       KC_MENU,    KC_0,    KC_6,    KC_5,    KC_4, XXXXXXX,    XXXXXXX, KC_MINS, _______, _______, KC_BSLS, XXXXXXX,
+       KC_MENU,    KC_0,    KC_6,    KC_5,    HR_4, XXXXXXX,    XXXXXXX, HR_MINS, _______, _______, KC_BSLS, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   _______,  KC_SPC, _______,    _______,  KC_ENT
   //                            ╰───────────────────────────╯ ╰──────────────────╯
