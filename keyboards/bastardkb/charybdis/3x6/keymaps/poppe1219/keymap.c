49b46255ec3ab7ee1a397858d43085075255fb44
poppe1219 <robin.asen@gmail.com>
2026-01-23 20:53:22 +0100

    Buggfix include

--- keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/poppe1219/keymap.c
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include swedish.h
+#include "swedish.h"
 
 
 enum charybdis_keymap_layers {
